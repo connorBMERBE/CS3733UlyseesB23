5cba47d21b19db7f7119531e24b312e44b190c5c
Atharva Tiwari <atharva.tiwari1124@gmail.com>
2023-11-20 23:45:38 -0500

    Login UI Working

--- seats4u/src/Pages/Landing.js
+++ seats4u/src/Pages/Landing.js
@@ -4,6 +4,8 @@ import React from 'react';
 export const Landing = () => {
 
     const [searchBarActive, setSearchBarActive] = React.useState(false);
+    const [loginPopupVisible, setLoginPopupVisible] = React.useState(false);
+
     const toggleSearchBar = () => {
         setSearchBarActive(!searchBarActive);
     }
@@ -13,12 +15,20 @@ export const Landing = () => {
         search.value = '';
     }
 
+    const showLoginPopup = () => {
+        setLoginPopupVisible(true);
+    }
+
+    const hideLoginPopup = () => {
+        setLoginPopupVisible(false);
+    }
+
     return (
     <main>
         <body className = "landingBody">
             <div className = "navBar">
-                <p>Admin Login</p>
-                <p>Venue Manager Login</p>
+                <p onClick={showLoginPopup}>Admin Login</p>
+                <p onClick={showLoginPopup}>Venue Manager Login</p>
                 <p className = "no-hover"> Seats4You </p>
             </div>
 
@@ -28,6 +38,22 @@ export const Landing = () => {
                     <input type="text" placeholder="Search for Shows" id = "consumerSearch"/>
                 </div>
                 <span className = "clear" onClick={clearSearchBar}/>
+            </div>
+
+            <div className ={`wrapper ${loginPopupVisible ? 'active' : ''}`}>
+                <div className = "loginWrapper">
+                    <span className = "closeIcon" onClick={hideLoginPopup}>&#10006;</span>
+                    <h2> Login </h2>
+                    <form action = "#">
+                        <div className = "inputBox">
+                            <input type="text" placeholder = "Username" required/>
+                        </div>
+                        <div className = "inputBox">
+                            <input type="password" placeholder = "Password" required/>
+                        </div>
+                        <button type="submit" className="loginButton"> Login </button>
+                    </form>
+                </div>
             </div>
         </body>
     </main>);
