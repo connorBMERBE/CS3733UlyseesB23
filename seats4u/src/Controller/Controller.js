59c3f91a745db89bb1da57e70d6e384d60e68f9f
connorBMERBE <89992394+connorBMERBE@users.noreply.github.com>
2023-11-30 19:07:35 -0500

    Update Controller.js

--- seats4u/src/Controller/Controller.js
+++ seats4u/src/Controller/Controller.js
@@ -32,4 +32,39 @@ export async function login(username, password) {
         });
 }
 
+//Function responsible for making show
+export async function makeShow(username, showName, date, time, venueName) {
+    Axios.post('https://j1e9gw8669.execute-api.us-east-1.amazonaws.com/Initial/login', {
+        'showName' : showName,
+        'date' : date,
+        'time' : time,
+        'venuName' : venueName,
+
+
+    }).then(function (response) {
+        console.log(response);
+        if (response.data.statusCode === 200) {
+            console.log(response.data.body.token);
+            const userToken = response.data.body.token;
+            const userRole = response.data.body.role;
+
+            //Store the token in local storage.
+            localStorage.setItem('token', userToken);
+            localStorage.setItem('role', userRole);
+
+            console.log("DONE TOKEN ADDED");
+        } else {
+            console.log(JSON.stringify({
+                statusCode: response.data.statusCode, 
+                body: response.data.body}));
+        }
+        
+    }).catch(function (error) {
+        //Handle Login failure.
+        console.log('Authenticaion Error: ', JSON.stringify({
+            statusCode: 400, 
+            body: "Invalid Credentials"}));
+    });
+}
+
 
